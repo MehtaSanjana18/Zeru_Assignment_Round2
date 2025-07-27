3e1771ac3489384404045728d1f5dd4966775489
MehtaSanjana18 <142376588+MehtaSanjana18@users.noreply.github.com>
2025-07-27 21:58:50 +0530

    Delete wallet_risk_scoring_from_scratch.py

--- wallet_risk_scoring_from_scratch.py
+++
@@ -1,9 +0,0 @@
-# Wallet Input
-import pandas as pd
-
-
-wallets_url = "https://docs.google.com/spreadsheets/d/1ZzaeMgNYnxvriYYpe8PE7uMEblTI0GV5GIVUnsP-sBs/export?format=csv"
-wallet_df = pd.read_csv(wallets_url)
-wallets = wallet_df['wallet_id'].str.lower().tolist()
-wallets
-
